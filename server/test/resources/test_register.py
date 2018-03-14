8f9b13e36b8dd2ba77556fd203f6ad378876db0c
simon-dube <simon.dube@hotmail.com>
2018-03-14 01:30:10 -0400

    Test register refactored

--- server/test/resources/test_register.py
+++ server/test/resources/test_register.py
@@ -1,124 +1,123 @@
-# import pytest
-# import os
-# import copy
-# import json
-# from server import app
-# from server.resources.models.error_code_and_message import ErrorCodeAndMessageSchema
-# from server.common.error_codes_and_messages import (
-#     INVALID_API_KEY, UNAUTHORIZED, MISSING_API_KEY, USERNAME_ALREADY_EXISTS,
-#     UNEXPECTED_ERROR)
-# from server.test.utils import get_test_config, load_json_data
-# from server.test.fakedata.users import admin, standard_user, standard_user_2
-# from server.database.models.user import User
+import pytest
+import os
+import copy
+import json
+from server import app
+from server.resources.models.error_code_and_message import ErrorCodeAndMessageSchema
+from server.common.error_codes_and_messages import (
+    INVALID_API_KEY, UNAUTHORIZED, MISSING_API_KEY, USERNAME_ALREADY_EXISTS,
+    UNEXPECTED_ERROR)
+from server.test.utils import load_json_data
+from server.test.conftest import test_client, session
+from server.test.fakedata.users import admin, standard_user, standard_user_2
+from server.database.models.user import User
 
-# @pytest.yield_fixture
-# def test_config(tmpdir_factory):
-#     test_config = get_test_config()
-#     test_config.db.session.add(admin(True))
-#     test_config.db.session.add(standard_user(True))
-#     test_config.db.session.commit()
 
-#     root_directory = tmpdir_factory.mktemp('data')
-#     app.config['DATA_DIRECTORY'] = str(root_directory)
+@pytest.fixture(autouse=True)
+def test_config(tmpdir_factory, session):
+    session.add(admin(True))
+    session.add(standard_user(True))
+    session.commit()
 
-#     yield test_config
+    root_directory = tmpdir_factory.mktemp('data')
+    app.config['DATA_DIRECTORY'] = str(root_directory)
 
-#     test_config.db.drop_all()
 
-# @pytest.yield_fixture
-# def test_user():
-#     return {
-#         "username": standard_user_2().username,
-#         "password": standard_user_2().password
-#     }
+@pytest.yield_fixture
+def test_user():
+    return {
+        "username": standard_user_2().username,
+        "password": standard_user_2().password
+    }
 
-# class TestRegisterResource():
-#     def test_register_missing_api_key(self, test_config, test_user):
-#         response = test_config.test_client.post(
-#             "/users/register",
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
-#         assert error == MISSING_API_KEY
 
-#     def test_register_invalid_api_key(self, test_config, test_user):
-#         response = test_config.test_client.post(
-#             "/users/register",
-#             headers={"apiKey": "NOT_{}".format(admin().api_key)},
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
-#         assert error == INVALID_API_KEY
+class TestRegisterResource():
+    def test_register_missing_api_key(self, test_client, test_user):
+        response = test_client.post(
+            "/users/register",
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
+        assert error == MISSING_API_KEY
 
-#     def test_register_user_api_key(self, test_config, test_user):
-#         response = test_config.test_client.post(
-#             "/users/register",
-#             headers={"apiKey": standard_user().api_key},
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
-#         assert error == UNAUTHORIZED
+    def test_register_invalid_api_key(self, test_client, test_user):
+        response = test_client.post(
+            "/users/register",
+            headers={"apiKey": "NOT_{}".format(admin().api_key)},
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
+        assert error == INVALID_API_KEY
 
-#     def test_register_successful(self, test_config, test_user):
-#         response = test_config.test_client.post(
-#             "/users/register",
-#             headers={"apiKey": admin().api_key},
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         assert response.status_code == 204
-#         assert os.path.exists(
-#             os.path.join(app.config['DATA_DIRECTORY'], test_user["username"]))
+    def test_register_user_api_key(self, test_client, test_user):
+        response = test_client.post(
+            "/users/register",
+            headers={"apiKey": standard_user().api_key},
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
+        assert error == UNAUTHORIZED
 
-#     def test_register_successful_and_login(self, test_config, test_user):
-#         response = test_config.test_client.post(
-#             "/users/register",
-#             headers={"apiKey": admin().api_key},
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         assert response.status_code == 204
+    def test_register_successful(self, test_client, test_user):
+        response = test_client.post(
+            "/users/register",
+            headers={"apiKey": admin().api_key},
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        assert response.status_code == 204
+        assert os.path.exists(
+            os.path.join(app.config['DATA_DIRECTORY'], test_user["username"]))
 
-#         response = test_config.test_client.post(
-#             "/authenticate", data=json.dumps(test_user), follow_redirects=True)
+    def test_register_successful_and_login(self, test_client, test_user):
+        response = test_client.post(
+            "/users/register",
+            headers={"apiKey": admin().api_key},
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        assert response.status_code == 204
 
-#         assert response.status_code == 200
+        response = test_client.post(
+            "/authenticate", data=json.dumps(test_user), follow_redirects=True)
 
-#     def test_register_already_existing_username(self, test_config, test_user):
-#         response = test_config.test_client.post(
-#             "/users/register",
-#             headers={"apiKey": admin().api_key},
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         assert response.status_code == 204
+        assert response.status_code == 200
 
-#         response2 = test_config.test_client.post(
-#             "/users/register",
-#             headers={"apiKey": admin().api_key},
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         error = ErrorCodeAndMessageSchema().load(
-#             load_json_data(response2)).data
+    def test_register_already_existing_username(self, test_client, test_user):
+        response = test_client.post(
+            "/users/register",
+            headers={"apiKey": admin().api_key},
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        assert response.status_code == 204
 
-#         expected_error_code_and_message = copy.deepcopy(
-#             USERNAME_ALREADY_EXISTS)
-#         expected_error_code_and_message.error_message = expected_error_code_and_message.error_message.format(
-#             test_user["username"])
-#         assert error == expected_error_code_and_message
+        response2 = test_client.post(
+            "/users/register",
+            headers={"apiKey": admin().api_key},
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        error = ErrorCodeAndMessageSchema().load(
+            load_json_data(response2)).data
 
-#     def test_register_already_existing_user_folder(self, test_config,
-#                                                    test_user):
-#         os.mkdir(
-#             os.path.join(app.config['DATA_DIRECTORY'], test_user["username"]))
+        expected_error_code_and_message = copy.deepcopy(
+            USERNAME_ALREADY_EXISTS)
+        expected_error_code_and_message.error_message = expected_error_code_and_message.error_message.format(
+            test_user["username"])
+        assert error == expected_error_code_and_message
 
-#         response = test_config.test_client.post(
-#             "/users/register",
-#             headers={"apiKey": admin().api_key},
-#             data=json.dumps(test_user),
-#             follow_redirects=True)
-#         error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
-#         assert response.status_code == 500
-#         assert error == UNEXPECTED_ERROR
+    def test_register_already_existing_user_folder(self, test_client, session,
+                                                   test_user):
+        os.mkdir(
+            os.path.join(app.config['DATA_DIRECTORY'], test_user["username"]))
 
-#         already_existing_user = test_config.db.session.query(User).filter_by(
-#             username=test_user["username"]).first()
+        response = test_client.post(
+            "/users/register",
+            headers={"apiKey": admin().api_key},
+            data=json.dumps(test_user),
+            follow_redirects=True)
+        error = ErrorCodeAndMessageSchema().load(load_json_data(response)).data
+        assert response.status_code == 500
+        assert error == UNEXPECTED_ERROR
 
-#         assert not already_existing_user
+        already_existing_user = session.query(User).filter_by(
+            username=test_user["username"]).first()
+
+        assert not already_existing_user
